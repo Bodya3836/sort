a601894236460d3759960c3922fab25c0f2f7660
Bodya3836 <137771918+Bodya3836@users.noreply.github.com>
2023-08-29 19:49:44 +0300

    Add files via upload

--- clean_folder/clean_folder/clean.py
+++ clean_folder/clean_folder/clean.py
@@ -116,61 +116,23 @@ def get_folder_objects(root_path):
             except OSError:
                 pass
 
-def main(folder_path):
-    scan(folder_path)
-
-    for file in jpeg_files:
-        hande_file(file, folder_path, "JPEG")
-
-    for file in jpg_files:
-        hande_file(file, folder_path, "JPG")
-
-    for file in png_files:
-        hande_file(file, folder_path, "PNG")
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: file_sorted <folder_path>")
+        return
     
-    for file in svg_files:
-        hande_file(file, folder_path, "SVG")
+    folder_path = sys.argv[1]
+    sort_folder = sys.argv[1]
+    Known_extensions = sys.argv[1]
+    Unknown_extensions = sys.argv[1]
 
-    for file in mov_files:
-        hande_file(file, folder_path, "MOV")
+    known_extensions, unknow_extensios = sort_folder(folder_path)
 
-    for file in mp4_files:
-        hande_file(file, folder_path, "MP4")
+    print("Known extensions",Known_extensions)
+    print("Unknown extensions",Unknown_extensions)
+    print("Folders names",folder_path)
+    
 
-    for file in mp3_files:
-        hande_file(file, folder_path, "MP3")
-
-    for file in ogg_files:
-        hande_file(file, folder_path, "OGG")
-
-    for file in wav_files:
-        hande_file(file, folder_path, "WAV")
-
-    for file in amr_files:
-        hande_file(file, folder_path, "AMR")
-
-    for file in mkv_files:
-        hande_file(file, folder_path, "MKV")
-
-    for file in avi_files:
-        hande_file(file, folder_path, "AVI")
-
-    for file in txt_files:
-        hande_file(file, folder_path, "TXT")
-
-    for file in docx_files:
-        hande_file(file, folder_path, "DOCX")
-
-    for file in others:
-        hande_file(file, folder_path, "OTHERS")
-
-    for file in archives:
-        handle_archive(file, folder_path, "ARCHIVE")
-
-    get_folder_objects(folder_path)
 
 if __name__ == '__main__':
-    path = sys.argv[1]
-    print(f"Start in {path}")
-    arg = Path(path)
-    main(arg.resolve())
+    main()
